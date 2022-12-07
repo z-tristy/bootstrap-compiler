54f227a94d000dde8f5985b378a059fb3dd5c719
tristy <tristy@fstln.io>
2022-12-07 19:01:23 +0800

    ..

--- src/views/Editor.js
+++ src/views/Editor.js
@@ -98,7 +98,7 @@ function Editor(props) {
           style={{ border: "none" }}
           height="100%"
           width="100%" 
-          src={`https://staging-preview.vercel.app/`}
+          src={`https://staging-preview.vercel.app/api`}
         />
       }
       </div>
