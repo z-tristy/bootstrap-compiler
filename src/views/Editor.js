6e167f1f8ea371735641fb8a91cdfd5adf085ff7
tristy <tristy@fstln.io>
2022-12-09 12:27:47 +0800

    ..

--- src/views/Editor.js
+++ src/views/Editor.js
@@ -41,20 +41,20 @@ function Editor(props) {
   // },[])
 
 
-  useEffect(() => {
-    const ws = new WebSocket("wss://staging-preview.vercel.app/");
-    // const ws = new WebSocket("ws://192.168.10.3:8002/");
-    // http://192.168.10.3:8001/
-    ws.onopen = (e) => {
-      console.log("socket opened");
-    }
+  // useEffect(() => {
+  //   const ws = new WebSocket("wss://staging-preview.vercel.app/");
+  //   // const ws = new WebSocket("ws://192.168.10.3:8002/");
+  //   // http://192.168.10.3:8001/
+  //   ws.onopen = (e) => {
+  //     console.log("socket opened");
+  //   }
 
-    ws.onmessage = (e) => {
-      console.log(e.data);
-    }
+  //   ws.onmessage = (e) => {
+  //     console.log(e.data);
+  //   }
 
-    setSocket(ws);
-  }, [])
+  //   setSocket(ws);
+  // }, [])
 
   const ioChangeColor = useCallback(
     () => {
