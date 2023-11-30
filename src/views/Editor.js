7ac1860ebf660ac9232c29510cb41211244c4bed
tristy <tristy@fstln.io>
2023-11-30 17:40:01 +0800

    handle color data

--- src/views/Editor.js
+++ src/views/Editor.js
@@ -5,13 +5,7 @@ import { useCallback, useEffect, useState } from "react";
 function Editor(props) {
 
   const [socket, setSocket] = useState(null);
-  const [color, setColor] = useState({
-    primary: '#F20000',
-    secondary: '#747475',
-    inverse: '#F7F7F7',
-    white: '#ffffff',
-    black: '#000000'
-  });
+  const [color, setColor] = useState("");
 
   useEffect(() => {
     const ws = new WebSocket("wss://nodejs-production-89c8.up.railway.app/");
@@ -21,27 +15,21 @@ function Editor(props) {
     }
 
     ws.onmessage = (e) => {
+      console.log(e.data);
       console.log('客户端 接受 message');
     }
 
     setSocket(ws);
   }, [])
 
-  const updateData = useCallback(
+  const ioChangeColor = useCallback(
     () => {
       if (!socket) return;
-      socket.send(JSON.stringify({
-        color
-      }));
+      console.log(2)
+      socket.send(JSON.stringify({ colorChange: color }));
     },
     [color, socket],
   );
-
-  const handleColorUpdate = (value, name) => {
-    console.log(value)
-    console.log(name)
-    setColor({...color, [name]: value})
-  }
 
 
   return (
@@ -49,42 +37,14 @@ function Editor(props) {
       {/* <a href={`https://fastlane-sections.myshopify.com/admin/oauth/authorize?client_id=b523ef94f70673c1ce904310d923918d&scope=write_products,write_customers,write_draft_orders&redirect_uri=http://192.168.8.55:3010&state=${new Date().getTime()}`}>New Code Link</a> */}
       <div style={{ width: "300px", margin: "0px auto" }}>
         <Form
-          onSubmit={updateData}
+
+          onSubmit={ioChangeColor}
         >
           <br />
           <TextField
-            value={color.primary}
-            name='primary'
+            value={color}
             label="Primary"
-            onChange={ e => handleColorUpdate(e, 'primary') }
-          />
-          <br />
-          <TextField
-            value={color.secondary}
-            name="secondary"
-            label="Secondary"
-            onChange={ e => handleColorUpdate(e, 'secondary') }
-          />
-          <br />
-          <TextField
-            value={color.inverse}
-            name="inverse"
-            label="inverse"
-            onChange={ e => handleColorUpdate(e, 'inverse') }
-          />
-          <br />
-          <TextField
-            value={color.white}
-            name="white"
-            label="white"
-            onChange={ e => handleColorUpdate(e, 'white') }
-          />
-          <br />
-          <TextField
-            value={color.black}
-            name="black"
-            label="black"
-            onChange={ e => handleColorUpdate(e, 'age') }
+            onChange={val => setColor(val)}
           />
 
           {/* <Button
