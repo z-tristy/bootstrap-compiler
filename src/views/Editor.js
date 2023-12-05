15455c0c67b4cea937adfb0e82888bc564e33394
tristy <tristy@fstln.io>
2023-12-05 14:07:27 +0800

    handle color

--- src/views/Editor.js
+++ src/views/Editor.js
@@ -1,21 +1,50 @@
 
-import { Form, TextField } from "@shopify/polaris";
-import { useCallback, useEffect, useState } from "react";
+import { Form, TextField, Page, Badge } from "@shopify/polaris"
+import { useCallback, useEffect, useState } from "react"
 
 function Editor(props) {
 
-  const [socket, setSocket] = useState(null);
+  const [socket, setSocket] = useState(null)
   const [general, setGeneral] = useState({
     primary: '#F20000',
     secondary: '#747475',
     inverse: '#F7F7F7',
     white: '#ffffff',
     black: '#000000'
-  });
+  })
+
+  const [maps, setMaps] = useState({
+    spacers: {
+      unit: 'rem',
+      data: {
+        0: 0,
+        1: .25, // 4
+        2: .5, // 8
+        3: 1, //16
+        4: 1.5, //24
+        5: 2, //32
+        6: 3, //48
+        7: 4, //64
+        8: 5, //80
+        9: 6, //96
+      }
+    },
+    'grid-breakpoints': {
+      unit: 'px',
+      data: {
+        xs: 0,
+        sm: 576,  // 576px
+        md: 768,  // 768px
+        lg: 1152, // 1152px
+        xl: 1280, // 1280px
+        xxl: 1440 // 1440px
+      }
+    }
+  })
 
   useEffect(() => {
-    // const ws = new WebSocket("ws://192.168.11.113:3000/");
-    const ws = new WebSocket("wss://nodejs-production-89c8.up.railway.app/");
+    const ws = new WebSocket("ws://192.168.11.113:3000/");
+    // const ws = new WebSocket("wss://nodejs-production-89c8.up.railway.app/");
     console.log(ws)
     ws.onopen = (e) => {
       console.log("socket opened");
@@ -33,23 +62,42 @@ function Editor(props) {
     () => {
       if (!socket) return;
       const generalString = stringifyGeneral()
-      console.log(generalString)
-      console.log('generalString')
+      const mapsString = stringifyMaps()
+      console.log(mapsString)
+      console.log('mapsString')
       socket.send(JSON.stringify({ 
-        general: generalString
+        general: `${generalString}${mapsString}`
       }));
     },
-    [general, socket],
+    [general, maps, socket],
   );
 
   const stringifyGeneral = () => {
-    let string = ''
+    let catchString = ''
     console.log(general)
     for (let key in general) {
       // if () 判断值不为空
-      string += `$${key}: ${general[key]};`
+      catchString += `$${key}: ${general[key]};`
     }
-    return string
+    return catchString
+  }
+
+  const stringifyMaps = () => {
+    let catchString = ''
+    console.log(maps)
+    for (let key in maps) {
+      // if () 判断值不为空
+      catchString += `$${key}: (`
+      const data = maps[key].data
+      if (typeof data === 'object' && !Array.isArray(data)) {
+        for (let k in data) {
+          catchString += `${k}: ${data[k]}${maps[key].unit},`
+        }
+      }
+      catchString += ');'
+      console.log(catchString)
+    }
+    return catchString
   }
 
   const handleGeneralUpdate = (value, name) => {
@@ -57,50 +105,89 @@ function Editor(props) {
     console.log(general)
   }
 
+  /**
+   * @param value 输入框修改的值
+   * @param key 参数类型, eg: spacers
+   * @param name 参数某类型下面的 key 名字
+  */
+  const handleMapsUpdate = (value, name, key) => {
+    console.log(name)
+    let catchs = maps[key]
+    let catchData = catchs.data
+
+    let convertValue = value / (catchs.unit === 'rem' ? 16 : 1)
+
+    catchData = {...catchData, [name]: convertValue}
+    catchs = {...catchs, data: catchData}
+    console.log(catchs)
+    console.log(catchData)
+    setMaps({...maps, [key]: catchs})
+  }
+
 
   return (
     <div style={{ width: "100%", height: "100%", display: "flex", flexDirection: "column" }}>
       {/* <a href={`https://fastlane-sections.myshopify.com/admin/oauth/authorize?client_id=b523ef94f70673c1ce904310d923918d&scope=write_products,write_customers,write_draft_orders&redirect_uri=http://192.168.8.55:3010&state=${new Date().getTime()}`}>New Code Link</a> */}
-      <div style={{ width: "300px", margin: "0px auto" }}>
+      <div style={{ width: "500px", margin: "0px auto" }}>
         <Form
-
           onSubmit={updateData}
         >
+          {
+            Object.keys(general).map(key =>
+              <div key={key}>
+                <br />
+
+                <TextField
+                  value={general[key]}
+                  name={key}
+                  label={key}
+                  onChange={ e => handleGeneralUpdate(e, key) }
+                />
+              </div>
+            )
+          }
+          {
+            Object.keys(maps).map(key =>
+              <div key={key}>
+                <br />
+                <br />
+                <h3>{key}</h3>
+
+                {
+                  Object.keys(maps[key].data).map(k =>
+                    <div key={k}>
+                      <br />
+                      <TextField
+                        value={String(maps[key].data[k] * (maps[key].unit === 'rem' ? 16 : 1))}
+                        type="number"
+                        name={maps[key].data[k]}
+                        label={k}
+                        onChange={ e => handleMapsUpdate(e, k, key) }
+                      />
+                    </div>
+                  )
+                }
+              </div>
+            )
+          }
+
+          {/* <br />
           <br />
-          <TextField
-            value={general.primary}
-            name='primary'
-            label="Primary"
-            onChange={ e => handleGeneralUpdate(e, 'primary') }
-          />
-          <br />
-          <TextField
-            value={general.secondary}
-            name="secondary"
-            label="Secondary"
-            onChange={ e => handleGeneralUpdate(e, 'secondary') }
-          />
-          <br />
-          <TextField
-            value={general.inverse}
-            name="inverse"
-            label="inverse"
-            onChange={ e => handleGeneralUpdate(e, 'inverse') }
-          />
-          <br />
-          <TextField
-            value={general.white}
-            name="white"
-            label="white"
-            onChange={ e => handleGeneralUpdate(e, 'white') }
-          />
-          <br />
-          <TextField
-            value={general.black}
-            name="black"
-            label="black"
-            onChange={ e => handleGeneralUpdate(e, 'black') }
-          />
+          <h3>Grid-breakpoints</h3>
+          {
+            Object.keys(maps['grid-breakpoints']).map(key =>
+              <div key={key}>
+                <br />
+                <TextField
+                  value={String(maps['grid-breakpoints'][key])}
+                  type="number"
+                  name={maps['grid-breakpoints'][key]}
+                  label={key}
+                  onChange={ e => handleMapsUpdate(e, key, 'grid-breakpoints') }
+                />
+              </div>
+            )
+          } */}
 
           {/* <Button
           fullWidth
